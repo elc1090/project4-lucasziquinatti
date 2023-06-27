3f2430bc934df4df77a83162f86d64541e72f6ef
Ziquinatti <72109329+Ziquinatti@users.noreply.github.com>
2023-06-26 21:13:54 -0300

    1ª versão do jogo

---
+++ client/src/components/UnityGame/index.jsx
@@ -0,0 +1,21 @@
+import { Unity, useUnityContext } from "react-unity-webgl";
+
+export default function UnityGame(){
+    const { unityProvider } = useUnityContext({
+        loaderUrl: "/unity/2.loader.js",
+        dataUrl: "/unity/2.data",
+        frameworkUrl: "/unity/2.framework.js",
+        codeUrl: "/unity/2.wasm",
+    })
+
+    return(
+        <div>
+            <Unity 
+                unityProvider={unityProvider}
+                width={960}
+                height={540}
+                style={{ width: '960px', height: '540px', background: '#1F1F20' }}
+            />
+        </div>
+    );
+}
