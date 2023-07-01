d6fca4a653c615e1984a7098f8ca4c275ba51a35
Ziquinatti <72109329+Ziquinatti@users.noreply.github.com>
2023-07-01 16:34:05 -0300

    Deploy Front

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:5000',
+    // baseURL: 'http://localhost:5000',
+    baseURL: 'https://api-zumbi-game-faa13d2d6e39.herokuapp.com',
 });
 
 export const registerAccount = async (username, password) => {
